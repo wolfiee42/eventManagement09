99d27ae99b6c01eb3a1d50388686f40253c31bc8
wolfiee42 <saifalislam2022@gmail.com>
2023-10-07 14:04:51 +0600

    footer section added

--- src/Components/Pages/layouts/Home.jsx
+++ src/Components/Pages/layouts/Home.jsx
@@ -1,6 +1,7 @@
 import Aboutus from "../../sublayouts/Aboutus";
 import Banner from "../../sublayouts/Banner";
 import Faq from "../../sublayouts/FAQ";
+import Footer from "../../sublayouts/Footer";
 
 const Home = () => {
     return (
@@ -8,6 +9,7 @@ const Home = () => {
             <Banner></Banner>
             <Aboutus></Aboutus>
             <Faq></Faq>
+            <Footer></Footer>
         </div>
     );
 };
---
+++ src/Components/sublayouts/Footer.jsx
@@ -0,0 +1,24 @@
+import { AiFillFacebook,AiFillTwitterSquare, AiFillYoutube } from "react-icons/ai";
+
+const Footer = () => {
+    return (
+        <footer className="bg-gradient-to-r from-gray-200 to-gray-700 footer footer-center p-5  text-primary-content">
+            <aside>
+                <img className="w-[200px] h-[100px]" src="https://i.ibb.co/L0GXprK/Screenshot-239-removebg-preview.png" alt="" />
+                <p className="font-semibold text-black">
+                    Wolfie Industries Ltd.
+                    <br />
+                    Copyright © 2023 - All right reserved</p>
+            </aside>
+            <nav>
+                <div className="grid grid-flow-col gap-4">
+                    <a href="https://www.facebook.com/JhankarMahbub"><AiFillFacebook className="w-8 h-8"></AiFillFacebook></a>
+                    <a href="https://www.youtube.com/@JhankarMahbub/featured"><AiFillYoutube className="w-8 h-8"></AiFillYoutube></a>
+                    <a href="https://twitter.com/jhankar_mahbub"><AiFillTwitterSquare className="w-8 h-8"></AiFillTwitterSquare></a>
+                </div>
+            </nav>
+        </footer>
+    );
+};
+
+export default Footer;
